1f95ab0dce5e183c679162e7c13e304cbde296c4
Michael Nutt <michaelnutt2@my.unt.edu>
2019-04-06 14:14:50 -0500

    Changed SDK version

--- app/src/main/java/com/example/unthackathon/MainActivity.kt
+++ app/src/main/java/com/example/unthackathon/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.unthackathon
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupBluetooth() {
+        val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
+            putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 0)
+        }
+        startActivity(discoverableIntent)
     }
 }
