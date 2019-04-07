80bc148cd35ce4cc241ada40c9f610e2fe751b6a
Michael Nutt <michaelnutt2@my.unt.edu>
2019-04-06 23:29:06 -0500

    Stuff

--- app/src/main/java/com/example/unthackathon/CommsActivity.kt
+++ app/src/main/java/com/example/unthackathon/CommsActivity.kt
@@ -8,22 +8,38 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class CommsActivity : AppCompatActivity() {
 
     private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private val deviceNameList = mutableListOf<String>("No names found")
+    private val deviceAddressList = mutableListOf<String>("Empty")
+    private lateinit var listview : ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_people)
+        setContentView(R.layout.activity_comms)
+
+        bluetoothAdapter?.startDiscovery()
+
+        deviceNameList.add("Test")
+
+        // Register for broadcasts when a device is discovered
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+
     }
 
     override fun onStart() {
         super.onStart()
 
-        // Register for broadcasts when a device is discovered
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
+        listview = findViewById<ListView>(R.id.comms_List)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceNameList)
+        listview.adapter = adapter
+
+        bluetoothAdapter?.cancelDiscovery()
     }
 
     // Create a BroadcastReceiver for ACTION_FOUND
@@ -33,10 +49,15 @@ class CommsActivity : AppCompatActivity() {
             val action: String = intent.action
             when (action) {
                 BluetoothDevice.ACTION_FOUND -> {
+                    //if(deviceAddressList[0] == "Empty") {
+                    //    deviceAddressList.removeAt(0)
+                    //    deviceNameList.removeAt(0)
+                    //}
                     // Discovery has found a device. Get the BluetoothDevice object and it's info from the Intent
                     val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
-                    val deviceName = device.name
-                    val deviceHardwareAddress = device.address // MAC Address
+                    deviceNameList.add("Test2")
+                    deviceNameList.add(device.name)
+                    deviceAddressList.add(device.address) // MAC Address
                 }
             }
         }
--- app/src/main/java/com/example/unthackathon/MainActivity.kt
+++ app/src/main/java/com/example/unthackathon/MainActivity.kt
@@ -11,19 +11,26 @@ import android.widget.Switch
 class MainActivity : AppCompatActivity() {
 
     private val bluetoothAdapter : BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-    // Pulls preferences
-    private val sharedPreferences = getSharedPreferences("unt_hackathon_preferences", Context.MODE_PRIVATE)
-    private val editor = sharedPreferences.edit()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Pulls preferences
+        val sharedPreferences = getSharedPreferences("unt_hackathon_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         editor.putString("bluetoothName", bluetoothAdapter.getName())
+        editor.commit()
     }
 
     override fun onStart() {
         super.onStart()
+
+        // Pulls preferences
+        val sharedPreferences = getSharedPreferences("unt_hackathon_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
 
         // Sets up buttons as variables
         val commsButton = findViewById<Button>(R.id.main_commsButton)
@@ -31,9 +38,6 @@ class MainActivity : AppCompatActivity() {
         val emSwitch = findViewById<Switch>(R.id.main_EMSwitch)
 
         emSwitch.isChecked = sharedPreferences.getBoolean("em_switch", false)
-        editor.putString("bluetoothName", bluetoothAdapter.getName())
-        editor.apply()
-        editor.commit()
 
         // Checks if switch changed, starts BT discovery if it is
         emSwitch.setOnCheckedChangeListener {buttonView, isChecked ->
@@ -42,7 +46,6 @@ class MainActivity : AppCompatActivity() {
             } else {
                 turnOffEM()
             }
-            editor.apply()
             editor.commit()
         }
 
@@ -60,6 +63,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupBluetooth() {
+        // Pulls preferences
+        val sharedPreferences = getSharedPreferences("unt_hackathon_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
 
         val discoverableIntent: Intent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE).apply {
             putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3600)
@@ -68,19 +74,19 @@ class MainActivity : AppCompatActivity() {
 
         bluetoothAdapter.setName("guardNet")
         editor.putBoolean("em_switch", true)
+        editor.commit()
     }
 
     private fun turnOffEM() {
+        // Pulls preferences
+        val sharedPreferences = getSharedPreferences("unt_hackathon_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
         val name = sharedPreferences.getString("bluetoothName","guardNet")
         bluetoothAdapter.setName(name)
         bluetoothAdapter.disable()
         editor.putBoolean("em_switch", false)
-        editor.apply()
         editor.commit()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        turnOffEM()
-    }
 }
